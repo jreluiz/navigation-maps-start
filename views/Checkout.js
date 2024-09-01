6429acf828fa45c24cad3f72cf2a4f7475dfed04
Jorge Luiz <jre-jl@hotmail.com>
2024-09-01 17:29:19 -0300

    Aula 2 - Finalizando criação do serviço referente ao backend.

--- views/Checkout.js
+++ views/Checkout.js
@@ -1,7 +1,36 @@
+import { URL_ROOT } from '@env'
 import React, { useState, useEffect, useRef } from 'react';
 import {Text, View, TouchableOpacity, StyleSheet} from 'react-native';
 
 export default function Checkout(props) {
+
+  useEffect(() => {
+    async function sendServer(){
+      console.log('Efetuando requisição...')
+      await fetch(URL_ROOT, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          price: props.route.params.price,
+          address: props.route.params.address,
+        })
+      }).then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json()
+      }).then(data => {
+        console.log(data)
+      }).catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+      })
+    }
+    sendServer()
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>O valor da corrida é: {props.route.params.price}</Text>
